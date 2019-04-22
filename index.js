1ca656c5415746601bb2ffcf5cdd96ee474f3095
jawad727 <jawaddhussein@gmail.com>
2019-04-22 14:06:54 -0700

    Heroku test 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server.js')
 
-const port = 4000
+const port = process.env.PORT || 4000 
 
 server.listen(port, () => {
    console.log(`server running on localhost${port}`)
